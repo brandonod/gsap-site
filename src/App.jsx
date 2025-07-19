17e1b3ad4870e6e9eaa0c64315e49e0b96044504
Brandon O'Donoghue <brandon@brandch.com>
2025-07-19 14:01:20 -0600

    Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import gsap from "gsap";
 import React from "react";
 import { ScrollTrigger } from "gsap/all";
 import { SplitText } from "gsap/all";
+import Navbar from "./Components/Navbar";
 
 // Remove SplitText from registerPlugin since it's not imported
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, GSAP!</h1>
-    </div>
+   <main>
+     <Navbar />
+   </main>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { navLinks } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        })
+    })
+
+
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="logo" />
+                    <p>Velvet Pour</p>
+                </a>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                         <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
