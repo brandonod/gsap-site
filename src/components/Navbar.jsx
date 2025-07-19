fe847a50bffda1a9b04ed5cd48c56f178befbb09
Brandon O'Donoghue <brandon@brandch.com>
2025-07-19 14:05:34 -0600

    Navbar GSAP animation finalized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,20 @@ import gsap from "gsap";
 
 const Navbar = () => {
     useGSAP(() => {
-        const navTween = gsap.timeline({
-            scrollTrigger: {
-                trigger: 'nav',
-                start: 'bottom top'
-            }
-        })
-    })
-
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: '#00000050',
+	 backgroundFilter: 'blur(10px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
 
     return (
         <nav>
