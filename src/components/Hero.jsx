076d179e8809be32794db6ca032b51b6e9157e67
Brandon O'Donoghue <brandon@brandch.com>
2025-07-20 13:43:08 -0600

    Animated leaves parallax affect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,6 +22,26 @@ const Hero = () => {
             ease: "expo.out",
             stagger: 0.06,
             });
+
+            gsap.from(paragraphSplit.lines, {
+                opacity: 0,
+                yPercent: 100,
+                duration: 1.8,
+                easte: 'expo.out',
+                stagger: 0.06,
+                delay: 1,
+            });
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: '#hero',
+                    start: 'top top',
+                    end: 'bottom top',
+                    scrub: true,
+                }
+            })
+            .to('.right-leaf' , {y: 200 } , 0)
+            .to('.left-leaf' , {y: -200 } , 0)
     }, []);
 
 
