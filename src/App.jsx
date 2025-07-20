c45b6a70da299c08af67183c2d0dd4ddae4e5120
Brandon O'Donoghue <brandon@brandch.com>
2025-07-20 13:30:35 -0600

    header text aniamted

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { ScrollTrigger } from "gsap/all";
 import { SplitText } from "gsap/all";
 import Navbar from "./Components/Navbar";
+import Hero from "./components/Hero";
 
 // Remove SplitText from registerPlugin since it's not imported
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -11,6 +12,7 @@ const App = () => {
   return (
    <main>
      <Navbar />
+     <Hero />
    </main>
   )
 }
